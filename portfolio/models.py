caff806119bf4f24041d4a43d568b40336a65b4e
Victor <gamerguyxxx123@gmail.com>
2024-03-12 21:52:22 +0100

    Prøve med database mellem portfolio og stocks

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from stocks.models import Stock
 
 # Create your models here.
+
+class Portfolio(models.Model):
+    userID = models.CharField(max_length=255)
+    stockID = models.CharField(max_length=255)
+    # Hvis en sector slettes, slettes alle aktier i den også
+    quantity = models.IntegerField(max_length=255)
+    buyingPrice = models.IntegerField(max_length=255)
+
+    #Hvis en stock slettes, slettes alle stocks i portfolien også
+    Stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
