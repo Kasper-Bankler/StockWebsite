72b98a9da439b5ae5bcfa45a2bfa534d803b1675
Victor <gamerguyxxx123@gmail.com>
2024-03-13 12:17:58 +0100

    login required og template til portfolio

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Orders
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
 def index(request):
     # Render templaten index.html
     #return render(request, "portfolio/index.html", {"portfolio": top_100_companies}
-    dountchokolade = Orders.objects.all
-    return render(request, "index2.html", {"portfolio": dountchokolade})
+    
+    orders = Orders.objects.all
+    return render(request, "index2.html", {"portfolio": orders})
