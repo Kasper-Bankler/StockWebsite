60cb120c3f7cb67b0914cf83fe1a19d497e2ec46
Victor <gamerguyxxx123@gmail.com>
2024-03-13 11:14:37 +0100

    Orders database

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from stocks.models import Stock
+from accounts.models import CustomUser
+from django.utils import timezone
+
 
 # Create your models here.
 
-class Portfolio(models.Model):
-    userID = models.CharField(max_length=255)
-    stockID = models.CharField(max_length=255)
-    # Hvis en sector slettes, slettes alle aktier i den også
-    quantity = models.IntegerField(max_length=255)
-    buyingPrice = models.IntegerField(max_length=255)
-
+class Orders(models.Model):
+    
+    quantity = models.IntegerField(max_length=255, default=0)
+    transaction_date = models.DateField(max_length=255, default=0)
+    isBuyOrder = models.BooleanField(max_length=255, default=True)
+    price = models.FloatField(max_length=255, default=0)
+    isActive = models.BooleanField(max_length=255, default=True)
     #Hvis en stock slettes, slettes alle stocks i portfolien også
-    Stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    
+    stockID = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    userID = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Portfolio
+from .models import Orders
 
 # Create your views here.
 def index(request):
     # Render templaten index.html
     #return render(request, "portfolio/index.html", {"portfolio": top_100_companies}
-    portfolio = Portfolio.objects.all
-    return render(request, "index2.html", {"portfolio": Portfolio})
+    dountchokolade = Orders.objects.all
+    return render(request, "index2.html", {"portfolio": dountchokolade})
