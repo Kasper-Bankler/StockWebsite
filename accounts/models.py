fa7b34a03ed64fc7978fb5d8afe1379bbe106776
Ali <39883081+goorekt@users.noreply.github.com>
2024-03-14 19:50:03 +0100

    IKKE MERE CHATGPT LEZGOOOO

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class CustomUser(AbstractUser):
     #app_label = "accounts"
     balance=models.FloatField(default=100000)
-    groups = models.ManyToManyField(Group, related_name='custom_user_groups')
-    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions')    
+    # groups = models.ManyToManyField(Group, related_name='custom_user_groups')
+    # user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions')    
     class Meta:
         app_label = 'accounts' 
