adadba8e84691b3f508f306d47fdc3d927e06b18
Victor <gamerguyxxx123@gmail.com>
2024-04-02 13:29:18 +0200

    Portfolio halvt fix (stadig error)

--- portfolio/views.py
+++ portfolio/views.py
@@ -9,14 +9,15 @@ import requests
 from plotly import graph_objects as go
 import pandas as pd
 from .models import Order
+from StockWebsite.utils import quicksort
 # Create your views here.
 
 
 @login_required
-def index(request):
+def index(request, sort = None):
     # Render templaten index.html
     # return render(request, "portfolio/index.html", {"portfolio": top_100_companies}
-    orders = list(Order.objects.filter(user=request.user,isActive=True))  # Henter alt data fra orders i databasen
+    orders = Order.objects.filter(user=request.user,isActive=True).values()  # Henter alt data fra orders i databasen
 
     #fetch = Order.objects.all() #Dette henter alt data fra order databasen
 
@@ -24,5 +25,20 @@ def index(request):
              #     {"fetch": fetch})
 
     # Render templaten index.html
+    results = list(orders)
+    
+    sorted_results = quicksort(results, 0, len(results)-1, 'stock', descending=True)
+
+    # Udvælg de 100 mest handlede aktier
+    top_100_companies = sorted_results[:100]
+
+    if sort == 'ticker':
+        orders = quicksort(top_100_companies, 0,
+                           len(top_100_companies)-1, 'T')
+    elif sort == 'price':
+        orders = quicksort(top_100_companies, 0,
+                           len(top_100_companies)-1, 'c', descending=True)
+    else:
+        orders = top_100_companies
     
     return render(request, "index.html", {"orders": orders})
