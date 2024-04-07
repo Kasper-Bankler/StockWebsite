aebb6457c6e1c9566572ebec82523a790c1be980
Victor <gamerguyxxx123@gmail.com>
2024-04-07 11:49:20 +0200

    sort by price i portfolio

--- portfolio/views.py
+++ portfolio/views.py
@@ -9,36 +9,18 @@ import requests
 from plotly import graph_objects as go
 import pandas as pd
 from .models import Order
-from StockWebsite.utils import quicksort
+from StockWebsite.utils import quicksort, linear_search
 # Create your views here.
 
-
 @login_required
-def index(request, sort = None):
-    # Render templaten index.html
-    # return render(request, "portfolio/index.html", {"portfolio": top_100_companies}
-    orders = Order.objects.filter(user=request.user,isActive=True).values()  # Henter alt data fra orders i databasen
+def index(request, sort=None):
 
-    #fetch = Order.objects.all() #Dette henter alt data fra order databasen
+    # Fetch orders from the database
+    orders = Order.objects.filter(user=request.user, isActive=True)
+    orders = list(orders.values())  # Convert queryset to list of dictionaries
 
-    #return render(request, "index.html",
-             #     {"fetch": fetch})
+    if sort == 'price':
+        # Sort orders by price using quicksort
+        quicksort(orders, 0, len(orders) - 1, "price", descending=True)
 
-    # Render templaten index.html
-    results = list(orders)
-    
-    sorted_results = quicksort(results, 0, len(results)-1, 'stock', descending=True)
-
-    # Udvælg de 100 mest handlede aktier
-    top_100_companies = sorted_results[:100]
-
-    if sort == 'ticker':
-        orders = quicksort(top_100_companies, 0,
-                           len(top_100_companies)-1, 'T')
-    elif sort == 'price':
-        orders = quicksort(top_100_companies, 0,
-                           len(top_100_companies)-1, 'c', descending=True)
-    else:
-        orders = top_100_companies
-    
     return render(request, "index.html", {"orders": orders})
