f8c7ed32b2d89987e20d63f69c2cde305f16fde8
Kasper Bankler <kasperbankler@gmail.com>
2024-04-08 13:46:59 +0200

    Update home page

--- StockWebsite/views.py
+++ StockWebsite/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-#from .models import Portfolio
+# from .models import Portfolio
 from django.http import HttpResponse
+from StockWebsite.utils import API_call, create_graph
 
 
 def home(request):
-    return render(request, 'home.html')
+    graph_data = API_call("https://api.polygon.io/v2/aggs/ticker/", "SPY",
+                          "/range/1/day/2000-01-01/2024-03-01?adjusted=true&sort=asc&limit=10000&apiKey=")
+
+    graph, price = create_graph(graph_data)
+
+    news = API_call(
+        "https://api.polygon.io/v2/reference/news?ticker=", "SPY", "&limit=3&apiKey=")
+
+    return render(request, 'home.html', {'graph': graph, 'news': news})
+
 
 def index(request):
     portfolio = portfolio.objects.all()
     return render(request, "index.html", {"portfolio": portfolio})
-
-
