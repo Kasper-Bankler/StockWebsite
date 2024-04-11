8a3dce6f345ce22023af28d00823c4f1eff0e46c
Kasper Bankler <kasperbankler@gmail.com>
2024-04-11 17:22:04 +0200

    Remove unused testcase

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -1,17 +1,1 @@
 from django.test import TestCase
-
-
-class YourTestClass(TestCase):
-    def setUp(self):
-        # Setup run before every test method.
-        pass
-
-    def tearDown(self):
-        # Clean up run after every test method.
-        pass
-
-    def test_something_that_will_pass(self):
-        self.assertFalse(False)
-
-    def test_something_that_will_fail(self):
-        self.assertTrue(False)
